a125cc1f4ac70e1e615c2cee397104b3f317837e
Pat Conley <pconley312@gmail.com>
2018-01-15 09:03:21 -0500

    auth0 login and logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent }        from './home/home.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
+import { CallbackComponent }    from './callback/callback.component';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'heroes', component: HeroesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',       component: HomeComponent },
+  { path: 'callback',   component: CallbackComponent }
+  { path: 'heroes',     component: HeroesComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard',  component: DashboardComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http'; 
+import { HttpModule } from '@angular/http'; // for auth0 ???
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -14,23 +16,29 @@ import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 
+import { AuthService } from './auth/auth.service';
+import { CallbackComponent } from './callback/callback.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule, HttpClientModule,
+    FormsModule, 
+    HttpClientModule,
+    // HttpModule, <<- the auth0 used this ???
     AppRoutingModule
   ],
-  providers: [ HeroService, MessageService, 
-    // MessageService 
-  ],
+  providers: [ AuthService, HeroService, MessageService, ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
