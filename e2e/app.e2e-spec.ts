eb2f825fa705792a6818f8c7b987619fc31b5568
Pat Conley <pconley312@gmail.com>
2018-01-18 07:22:20 -0500

    e2e first test fixed

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('heroes App', () => {
+describe('Kazoku Restart', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('heroes App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Kazoku Client Restart');
   });
 });
